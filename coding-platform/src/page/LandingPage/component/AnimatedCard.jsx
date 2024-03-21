08561f116ae3e405df65e3f617d9e87ade7cc053
mAnAgyA$1 <korwutacollins@gmail.com>
2024-03-21 21:14:15 +0000

    Landing page build 2/3

--- coding-platform/src/page/LandingPage/component/AnimatedCard.jsx
+++ coding-platform/src/page/LandingPage/component/AnimatedCard.jsx
@@ -1,14 +1,16 @@
 import style from "../LandingPagesStyle.module.css";
 import trophyImage from "../trophy-colored.svg";
+import competitionImage from '../collaboration-image.jpg'
 
 export default function AnimateCard(props) {
     return (
         <div className={`${style.competitionBox} ${props.animate==='left'?style.left:props.animate==='center'?
-        style.center:props.animate?style.right:''}`}>
-            <div className={style.competitionImageContainer}>
+        style.center:props.animate?style.right:''}`} >
+            <div className={style.competitionImageContainer} style={{backgroundImage:`url(${props.backgroundImage})`}}>
                 <img src={props.img} alt={props.alt}/>
             </div>
             <div className={style.competitionText}>
+                <span>{props.caption}</span>
                 <p>
                     {props.message}
                 </p>
